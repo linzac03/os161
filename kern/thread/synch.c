d00a2c5e0ccdc03e0be34f243ded1dddbd28134b
Your Name <linzac03@evergreen.edu>
2016-01-27 01:36:14 -0800

    locks and CVs are working, probably need to look into cleanitems function since it doesn't seem to be able to run tests more than once

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -251,6 +251,14 @@ cv_create(const char *name)
                 kfree(cv);
                 return NULL;
         }
+
+		  cv->cv_wchan = wchan_create(cv->cv_name);
+		  if (cv->cv_wchan == NULL) {
+		 	  kfree(cv->cv_name);
+			  kfree(cv);
+			  return NULL;
+		  }
+
         
         // add stuff here as needed
         
@@ -263,7 +271,7 @@ cv_destroy(struct cv *cv)
         KASSERT(cv != NULL);
 
         // add stuff here as needed
-        
+		  wchan_destroy(cv->cv_wchan); 
         kfree(cv->cv_name);
         kfree(cv);
 }
@@ -271,23 +279,35 @@ cv_destroy(struct cv *cv)
 void
 cv_wait(struct cv *cv, struct lock *lock)
 {
-        // Write this
-        (void)cv;    // suppress warning until code gets written
-        (void)lock;  // suppress warning until code gets written
+	// Write this
+	if (lock_do_i_hold(lock)) {
+		wchan_lock(cv->cv_wchan);
+		lock_release(lock);
+		wchan_sleep(cv->cv_wchan);
+		lock_acquire(lock);
+	}
+	//(void)cv;    // suppress warning until code gets written
+	//(void)lock;  // suppress warning until code gets written
 }
 
 void
 cv_signal(struct cv *cv, struct lock *lock)
 {
         // Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	if (lock_do_i_hold(lock)){
+		wchan_wakeone(cv->cv_wchan);
+	}
+		  //(void)cv;    // suppress warning until code gets written
+	//(void)lock;  // suppress warning until code gets written
 }
 
 void
 cv_broadcast(struct cv *cv, struct lock *lock)
 {
 	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	if (lock_do_i_hold(lock)) {
+		wchan_wakeall(cv->cv_wchan);	
+	}
+	//(void)cv;    // suppress warning until code gets written
+	//(void)lock;  // suppress warning until code gets written
 }
